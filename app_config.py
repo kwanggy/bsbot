80b90f47dc38c5ecddd68b3335de429dc30fc1e4
devty <devty1023@gmail.com>
2013-12-28 01:27:00 -0500

    cleaning up

--- app_config.py
+++
@@ -1,6 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-MOCK_DB_URI = 'sqlite:///' + os.path.join(basedir, 'mock.db')
-CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
